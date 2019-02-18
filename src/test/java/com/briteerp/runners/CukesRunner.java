a9cff6985c9f05d78a4babd6e2c7807c56899f48
myilmaz66 <mmyilmaz66@gmail.com>
2019-02-18 10:38:42 -0500

    Update CukesRunner

--- src/test/java/com/briteerp/runners/CukesRunner.java
+++ src/test/java/com/briteerp/runners/CukesRunner.java
@@ -15,7 +15,6 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/briteerp/steps",
         dryRun = false)
-
 public class CukesRunner {
 
 }
